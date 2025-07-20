f72a10708214552b400607a92fa0061fdcad0671
Arham097 <arhamhasan70@gmail.com>
2025-07-20 13:10:11 +0500

    Basic GET,POST,PUT,DELETE Apis integrated

--- obj/Debug/net9.0/ASP.NET Core.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.NET Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f250c3dc803f284f5afc9f27c008eec81b7f2e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
